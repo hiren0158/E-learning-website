9254a161fc53027f1602cc4ec8c095a5a89c480b
hiren0158 <147523980+hiren0158@users.noreply.github.com>
2024-04-03 21:27:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.getElementById("learn-cpp").addEventListener("click", function () {
+    window.location.href = "learn_cpp.html";
+});
+
+
+document.getElementById("learn-java").addEventListener("click", function () {
+    window.location.href = "learn-java.html";
+});
+
+
+document.getElementById("learn-python").addEventListener("click", function () {
+    window.location.href = "learn-python.html";
+});
